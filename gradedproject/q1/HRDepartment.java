06b373b741bddc5bdebb3747cc9bfb9913903833
SGN31 <144659110+SGN31@users.noreply.github.com>
2023-10-30 11:19:01 +0530

    Add files via upload

---
+++ gradedproject/q1/HRDepartment.java
@@ -0,0 +1,23 @@
+package com.gradedproject.q1;
+
+public class HRDepartment extends SuperDepartment {
+
+    @Override
+    public String departmentName() {
+        return "HR Department";
+    }
+
+    @Override
+    public String getWorkDeadline() {
+        return "Complete by EOD";
+    }
+
+    @Override
+    public String getTodaysWork() {
+        return "Fill today’s timesheet and mark your attendance";
+    }
+
+    public String doActivity() {
+        return "Team Lunch";
+    }
+}
